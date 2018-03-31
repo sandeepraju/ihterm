6495f2249a439a3b8c669d699fd02b595dbb24d4
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-03-30 21:18:19 -0700

    ugly implementation to get all data

--- pkg/ihterm.go
+++ pkg/ihterm.go
@@ -1,21 +1,21 @@
 package pkg
 
 import (
-	"net/url"
-	"log"
-	"net/http"
 	"errors"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"net/url"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 const (
-	ihUrl = "https://www.indiehackers.com/"
-	ihPostSelector = "div.thread-row div.ember-view"
+	ihUrl                 = "https://www.indiehackers.com/"
+	ihPostSelector        = "div.thread-row div.ember-view"
 	ihPostUpvotesSelector = "div.thread-row.ember-view div.thread-voter.ember-view div.thread-voter__text div.thread-voter__count"
-	ihPostTitleSelector = "div.thread-row.ember-view div.thread__details a.thread__title.ember-view"
-
+	ihPostTitleSelector   = "div.thread-row.ember-view div.thread__details a.thread__title.ember-view"
 )
 
 // IHTerm ...
@@ -41,14 +41,35 @@ func (iht *IHTerm) Posts() ([]IHPost, error) {
 		return nil, err
 	}
 
-
 	// posts := make([]IHPost, 0)
 	fmt.Println("finding")
 	fmt.Println(doc.Find("div.homepage__thread-list").Children().Length())
-	doc.Find("div.homepage__thread-list").Children().Each(func(i int, s *goquery.Selection) {
+	doc.Find("div.homepage__thread-list").Children().Slice(0, 14).Each(func(i int, s *goquery.Selection) {
 		fmt.Println(i)
-		userName := strings.TrimSpace(s.Find("span.user-link__username").Text())
+		userName := strings.TrimSpace(s.Find("div.thread__metadata span.user-link__username").Text())
+		if userLink, ok := s.Find("div.thread__metadata div a").Attr("href"); ok {
+			// TODO: prepend with baseUrl
+			fmt.Println(userLink)
+		}
 		fmt.Println(userName)
+
+		postLink := s.Find("div.thread__metadata a.thread__date")
+		duration := postLink.Text()
+		fmt.Println(strings.TrimSpace(duration))
+		if postUrl, ok := postLink.Attr("href"); ok {
+			fmt.Println(postUrl)
+		}
+
+		commentCount := s.Find("div.thread__metadata a.thread__reply-count").Text()
+		fmt.Println(commentCount)
+
+		postTitle := s.Find("div.thread__details a.thread__title").Text()
+		fmt.Println(postTitle)
+
+		upvotes := s.Find("div.thread-voter div.thread-voter__text div.thread-voter__count").Text()
+		fmt.Println(upvotes)
+
+		fmt.Println("---")
 		// posts = append(posts, )
 		// For each item found, get the
 		// upvotes := s.Find(ihPostUpvotesSelector).Text()
@@ -61,12 +82,12 @@ func (iht *IHTerm) Posts() ([]IHPost, error) {
 }
 
 type IHPost struct {
-	Upvotes int
-	Title string
-	Url *url.URL
-	Author string
+	Upvotes    int
+	Title      string
+	Url        *url.URL
+	Author     string
 	ApproxTime string
-	Comments int
+	Comments   int
 }
 
 // NewIHTerm ...
